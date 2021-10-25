51dd5703f66a79cc252b3f77ef49444104b5a164
RubyCodesYT <81465414+RubyCodesYT@users.noreply.github.com>
2021-10-25 15:30:16 +1300

    Base identity component + integration with db

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import { useState, useEffect } from 'react';
 import React from 'react';
 import './App.css';
 import Identity from './component/Identity'
+import { db } from './db/firebase'
+import { collection, getDocs } from 'firebase/firestore'
 
 function App() {
+  const [definitions, setDefinitions] = useState([]);
+  const refDb = collection(db, "definitions");
+
+  useEffect(() => {
+    const getDefinitions = async () => {
+      const data = await getDocs(refDb);
+      const toMap : any[] = data.docs.map((doc) => ({...doc.data(), id: doc.id}))
+      setDefinitions((toMap as any));
+    }
+  }, []);
+
   return (
     <div className="App">
-      <Identity name="testing" description="Testing"></Identity>
+      {definitions.map((definition: any) => {
+        return (
+          <Identity name={definition.name} description={definition.description}/>
+        )
+      })}
     </div>
   );
 }
--- src/db/firebase.ts
+++ src/db/firebase.ts
@@ -1,4 +1,5 @@
-import { initializeApp } from "firebase/compat/app";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDBp9LrYJBTggoAapn5fCEPMUfFVHV9Xqk",
@@ -8,6 +9,7 @@ const firebaseConfig = {
     messagingSenderId: "273190810158",
     appId: "1:273190810158:web:96f25764c979bf9310e5bf",
     measurementId: "G-P40WGQ1LYE"
-}
+};
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
